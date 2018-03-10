e25535eedb436f25d429e161924a9fcff6baea2a
albin <padinju@gmail.com>
2018-03-10 15:19:58 +0530

    tables

---
+++ database/migrations/2018_03_10_094055_create_post_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('p_id')->unsigned();
+            $table->foreign('p_id')->references('post_id')->on('posts');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->mediumText('user_comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_comments');
+    }
+}
