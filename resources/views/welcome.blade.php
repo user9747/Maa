7297699be6bfa5d4a5322cf097909966822f7319
abhisheknambiar98 <abhisheknambiar@cet.ac.in>
2018-03-10 14:00:19 +0530

    video view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,4 @@
 @section('content')
 <link rel='stylesheet' href={{URL::to('src/css/welcome.css')}}>
 
-<div class="row">
-	<div class="col-md-12">
-		<img src={{URL::to('src/img/logo.jpeg')}} class="img-responsive" align="center">
-		<h1>Renegade</h1>
-		<a href="signup">  <button type="button" class="bton">Sign Up</button></a><br>
-		<a href="signin">  <button type="button" class="bton">Sign In</button></a>
-	</div>
-</div>
-
 @endsection
