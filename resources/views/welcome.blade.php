7a859eb092195ceaa0dd45fa0462584c1b50d2b9
albin <padinju@gmail.com>
2018-03-10 15:35:31 +0530

    bug fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 <div class="row full">
     <div class="col-md-6">
         <h2 class="vent">A venture to help out<br> all the gonna-be moms.</h2>
-        <p class="weldesc">Welcome to the start of your bump-tastic pregnancy journey! 
+        <p class="weldesc">Welcome to the start of your bump-tastic pregnancy journey!
         We at Project Maa are here to help you out through all the bumps and downs.</p>
     </div>
     <div class="col-md-6">
@@ -18,8 +18,8 @@
 		<img src="{{ asset('img/maalogo.png') }}" class="img-responsive logop">
         <h4 class="apptitle">Project Maa.</h4>
 
-		<a href="login">  <button type="button" class="bton">Sign Up</button></a><br>
-		<a href="register">  <button type="button" class="bton">Sign In</button></a>
+		<a href="register">  <button type="button" class="bton">Sign Up</button></a><br>
+		<a href="login">  <button type="button" class="bton">Sign In</button></a>
 	</div>
 </div>
 </div>
