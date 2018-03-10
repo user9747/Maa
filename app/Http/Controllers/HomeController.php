fd3fd0f7a0d47013aa96bff6a42b7402d0d5a2d3
Gopal <gopalkurup@gmail.com>
2018-03-10 22:19:50 +0530

    Albinte kazivu kedu sheri aaki

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -38,7 +38,7 @@ class HomeController extends Controller
         return view('home');
     }
     public function scheme(){
-      $schemes = scheme::orderBy('id','DESC');
+      $schemes = scheme::orderBy('created_at','desc')->paginate(10);
       return view('schemes')->with('schemes',$schemes);
 
     }
--- resources/views/schemes.blade.php
+++ resources/views/schemes.blade.php
@@ -5,18 +5,17 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
         <h1>Scheme</h1>
-            @if(count($schemes) > 0)
+            @if(count($schemes) > 0)               
                 @foreach($schemes as $scheme)
                     <div class = "well">
                         <div class="row">
                             <div class="col-md-8 col-xs-8">
-                                <h1><a href="/schemes/{{$scheme->id}}">{{$scheme->title}}</a></h1>
+                                <h1><a href="/scheme/{{$scheme->id}}">{{$scheme->title}}</a></h1>
                                 <small>Created on {{$scheme->created_at}}</small>
                             </div>
                         </div>
                     </div>
                 @endforeach
-                {{-- {{$scheme->links()}} --}}
             @else
                 <p>No schemes found</p>
             @endif
