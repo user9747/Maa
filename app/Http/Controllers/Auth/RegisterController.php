1af46fd2c2bc193e98c5c9558f0edb2575cfebb5
Gopal <gopalkurup@gmail.com>
2018-03-10 14:13:29 +0530

    Auth table succesfully oppichu

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,6 +51,8 @@ class RegisterController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
+            'usertype' => 'required',
+            'phnumber' => 'required|string',
         ]);
     }
 
@@ -66,6 +68,8 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'usertype' => 'woman',
+            'phnumber' => $data['phnumber'],
         ]);
     }
 }
