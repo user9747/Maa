2770d12e6471a420c7e51d9d1c3d2109fa629115
Gopal <gopalkurup@gmail.com>
2018-04-06 20:56:41 +0530

    Added size migration

---
+++ database/migrations/2018_04_06_151837_create_sizes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSizesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sizes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('week');
+            $table->string('size');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sizes');
+    }
+}
