2610a870e3e55c2c26a9ea4535e4e95def00b187
Gopal <gopalkurup@gmail.com>
2018-10-23 21:24:18 +0530

    Adding screenshots

--- resources/views/live.blade.php
+++ resources/views/live.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-        <iframe src="https://www.ustream.tv/embed/23548463?html5ui" style="border: 0 none transparent;"  webkitallowfullscreen allowfullscreen frameborder="no" width="720" height="405"></iframe>
+        <iframe src="https://www.ustream.tv/embed/23548463?html5ui" style="border: 0 none transparent;"  webkitallowfullscreen allowfullscreen frameborder="no" width="480" height="270"></iframe>
         <iframe src="https://www.ustream.tv/socialstream/23548463?videos=0" style="border: 0 none transparent;"  webkitallowfullscreen allowfullscreen frameborder="no" width="320" height="235"></iframe>
         </div>
     </div>
